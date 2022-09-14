9305dcd3134a0c57ed9ecaf2e924739b0dbc25a3
Alfredo Mariño <alfredo.marino@enzyme.biz>
2022-09-14 11:55:51 +0200

    example queries refact

--- db/beender_db_data.sql
+++ db/beender_db_data.sql
@@ -4,15 +4,39 @@
 -- bee
 
 INSERT INTO bee (firstname, lastname, interested_in, experience_years, picture, bio)
-VALUES ('Alfredo', 'Mariño', 'PYTHON_DEVELOPER', 2, 'ALKSDJFLAJSDF', 'Programador python')
+VALUES (
+    'Robert',
+    'De Niro',
+    'PYTHON_DEVELOPER',
+    2,
+    'https://imagenes.20minutos.es/files/image_990_v3/uploads/imagenes/2013/11/18/9a6b684__3.jpg',
+    'Programador python'
+);
 
 INSERT INTO bee (firstname, lastname, interested_in, experience_years, picture, bio)
-VALUES ('Natalie', 'Portman', 'DATA_SCIENTIST', 4, 'ALKSDJFLAJSDF', 'Cientfica de datos y actriz')
+VALUES (
+    'Julia',
+    'Roberts',
+    'DATA_SCIENTIST',
+    4,
+    'https://media.vogue.mx/photos/5ebee8ede45de519e6ce9155/4:3/w_1800,h_1350,c_limit/la-boda-de-mi-mejor-amigo-julia-roberts.jpeg',
+    'Cientfica de datos'
+);
 
 -- hive
 
 INSERT INTO hive (name, description, queen_bee, picture)
-VALUES ('Tecnology', 'Technology department', 'Freddie Mercury', 'akshdfaklsjdf')
+VALUES (
+    'Artemis',
+    'Desarrollo de aplicaciones en el ambito medico',
+    'Freddie Mercury',
+    'https://noticiassalamanca.com/wp-content/uploads/2022/07/shrek-cinta-estudios-dreamworks-ogro-1.jpeg'
+);
 
 INSERT INTO hive (name, description, queen_bee, picture)
-VALUES ('Machine Learning', 'Machine Learning department', 'Vin Diesel', 'akshdfaklsjdf')
+VALUES (
+    'AI For business',
+    'Machine Learning department',
+    'Vin Diesel',
+    'https://i.blogs.es/7f0194/power-rangers/450_1000.jpg'
+);
